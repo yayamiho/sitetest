65fd64777beb0d15294c4b40326ea56145f00507
Yasmin Miho Tsuruta da Silva <yasmin.m.t.dasilva@gmail.com>
2024-10-30 11:46:07 -0300

    Add files via upload

---
+++ javascript/test.js
@@ -0,0 +1,25 @@
+setInterval(showTime, 1000);
+
+function showTime() {
+    let time = new Date();
+    let hour = time.getUTCHours() - 3; // Adjust to GMT-3
+    let min = time.getUTCMinutes();
+    let sec = time.getUTCSeconds();
+
+    // Ensure hour is in the correct range (0-23)
+    if (hour < 0) {
+        hour += 24; // Adjust for negative hours
+    } else if (hour >= 24) {
+        hour -= 24; // Adjust for hours exceeding 24
+    }
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    let currentTime = hour + ":" + min + ":" + sec;
+
+    document.getElementById("headerInfoClock").innerHTML = currentTime;
+}
+
+showTime();
